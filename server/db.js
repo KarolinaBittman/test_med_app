917b08f05c95900c22126c11c6058d28e1aa9716
KarolinaBittman <karolina.bittman@gmail.com>
2025-11-27 09:57:20 -0500

    DB Setup: Updated db.js with MongoDB connection credentials.

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<4aF5YBlCgKNbyCA1ZTptnxIa>@172.21.185.55:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
